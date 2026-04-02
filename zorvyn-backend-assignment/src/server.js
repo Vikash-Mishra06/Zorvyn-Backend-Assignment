666c51d213cdf67c5a6d44b59ea8f1ced59dbfde
Vikash Mishra <mishravikash0612@gmail.com>
2026-04-02 17:44:21 +0530

    initial project setup with express server and folder structure

---
+++ zorvyn-backend-assignment/src/server.js
@@ -0,0 +1,11 @@
+import "dotenv/config.js";
+import app from "./app.js";
+import connectDB from "./db/db.js";
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
